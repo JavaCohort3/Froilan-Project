e0a1b5342166e90be5fd73fc5685b0a1f450441c
iVesperX <aermias@live.com>
2018-07-16 09:22:46 -0400

    initial commit

---
+++ src/main/java/io/javacohort3/FarmerFroilan/FarmerFroilanApplication.java
@@ -0,0 +1,12 @@
+package io.javacohort3.FarmerFroilan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmerFroilanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmerFroilanApplication.class, args);
+	}
+}
