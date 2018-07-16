62910971ce6fa6f6788373cfda7ea14da937c97c
iVesperX <aermias@live.com>
2018-07-16 09:35:40 -0400

    removed spring dependencies

--- src/main/java/io/javacohort3/FarmerFroilan/FarmerFroilanApplication.java
+++ src/main/java/io/javacohort3/FarmerFroilan/FarmerFroilanApplication.java
@@ -1,12 +1,7 @@
 package io.javacohort3.FarmerFroilan;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
 public class FarmerFroilanApplication {
-
 	public static void main(String[] args) {
-		SpringApplication.run(FarmerFroilanApplication.class, args);
+
 	}
 }
