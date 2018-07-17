95c4cfbf08a61b91feea3a0361f1c8f80ed6151a
Jermaine34343 <Jermaine34343@gmail.com>
2018-07-17 14:49:32 -0400

    Fixed CropDuster problems

--- src/main/java/io/javacohort3/FarmerFroilan/abstracts/CropDuster.java
+++ src/main/java/io/javacohort3/FarmerFroilan/abstracts/CropDuster.java
@@ -5,7 +5,15 @@ import io.javacohort3.FarmerFroilan.storage.CropRow;
 
 public abstract class CropDuster implements Aircraft {
 
+    public CropDuster() {
+    }
+
     public void fertilize(CropRow cropRow){
-        this.fertilize(cropRow);
+        System.out.println("Crops were fertilized " + cropRow);
+    }
+
+    @Override
+    public String toString() {
+        return "CropDuster{}";
     }
 }
