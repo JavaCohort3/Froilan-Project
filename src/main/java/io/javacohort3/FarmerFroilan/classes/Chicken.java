56802d700366903b773dd83fabb6b17e96e719fb
senorcl0ud <claudemcalpin@gmail.com>
2018-07-17 09:46:26 -0400

    accounted for future imports

--- src/main/java/io/javacohort3/FarmerFroilan/classes/Chicken.java
+++ src/main/java/io/javacohort3/FarmerFroilan/classes/Chicken.java
@@ -1,9 +1,14 @@
 package io.javacohort3.FarmerFroilan.classes;
 
+import io.javacohort3.FarmerFroilan.interfaces.Produce;
+import io.javacohort3.FarmerFroilan.abstracts.Animal;
+import io.javacohort3.FarmerFroilan.classes.EdibleEgg;
+
 public class Chicken extends Animal implements Produce {
 
     @Override
     public EdibleEgg yield(){
+        return new EdibleEgg();
     }
     //chicken can yeild edible eggs
 }
--- src/main/java/io/javacohort3/FarmerFroilan/classes/Horse.java
+++ src/main/java/io/javacohort3/FarmerFroilan/classes/Horse.java
@@ -1,4 +1,7 @@
 package io.javacohort3.FarmerFroilan.classes;
 
+import io.javacohort3.FarmerFroilan.abstracts.Animal;
+import io.javacohort3.FarmerFroilan.interfaces.Rideable;
+
 public class Horse extends Animal implements Rideable {
 }
