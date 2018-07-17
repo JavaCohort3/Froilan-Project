1bab641c0e5050d7a31c0acac3db6a98da0fa3bc
senorcl0ud <claudemcalpin@gmail.com>
2018-07-17 10:25:57 -0400

    deleted horse and chicken

--- src/main/java/io/javacohort3/FarmerFroilan/classes/Horse.java
+++
@@ -1,7 +0,0 @@
-package io.javacohort3.FarmerFroilan.classes;
-
-import io.javacohort3.FarmerFroilan.abstracts.Animal;
-import io.javacohort3.FarmerFroilan.interfaces.Rideable;
-
-public class Horse extends Animal implements Rideable {
-}
