a1a29c159760b9ff58be3cbcc36e3b0643b5110a
Jermaine34343 <Jermaine34343@gmail.com>
2018-07-17 12:56:14 -0400

    fixed Tractor

--- src/main/java/io/javacohort3/FarmerFroilan/abstracts/Tractor.java
+++ src/main/java/io/javacohort3/FarmerFroilan/abstracts/Tractor.java
@@ -4,9 +4,10 @@ import io.javacohort3.FarmerFroilan.abstracts.Crop;
 import io.javacohort3.FarmerFroilan.interfaces.FarmVehicle;
 
 public abstract class Tractor implements FarmVehicle {
+
     private Crop crop;
 
     public void harvest(Crop crop){
-        this.crop = crop;
+      this.crop = crop;
     }
 }
