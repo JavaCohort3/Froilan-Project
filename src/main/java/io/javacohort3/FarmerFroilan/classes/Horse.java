456d6f770731a58630b283de5a34ca16cc8f672a
iVesperX <aermias@live.com>
2018-07-17 12:11:58 -0400

    resolved errors in Chicken.java and Horse.java

--- src/main/java/io/javacohort3/FarmerFroilan/classes/Horse.java
+++ src/main/java/io/javacohort3/FarmerFroilan/classes/Horse.java
@@ -1,10 +1,15 @@
 package io.javacohort3.FarmerFroilan.classes;
 
 import io.javacohort3.FarmerFroilan.abstracts.Animal;
+import io.javacohort3.FarmerFroilan.abstracts.Edible;
 import io.javacohort3.FarmerFroilan.interfaces.Rideable;
 
 public class Horse extends Animal implements Rideable {
+    public void eat(Edible edible) {
+        System.out.println("Horse has eaten");
+    }
+
     public void makeNoise() {
-        System.out.println("bdd-ddd-dd-rrr");
+        System.out.println("watch me whip and neigh!!!");
     }
 }
