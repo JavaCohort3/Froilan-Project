7df3bcffed4dc4b36e20bebbb933f26aee017d9a
Jermaine34343 <Jermaine34343@gmail.com>
2018-07-17 14:57:14 -0400

    fixed CropDuster

--- src/main/java/io/javacohort3/FarmerFroilan/classes/CropDuster.java
+++ src/main/java/io/javacohort3/FarmerFroilan/classes/CropDuster.java
@@ -5,6 +5,7 @@ import io.javacohort3.FarmerFroilan.storage.CropRow;
 
 public class CropDuster implements Aircraft {
     public void fly() {
+        
     }
 
     public void fertilize(CropRow cropRow){
