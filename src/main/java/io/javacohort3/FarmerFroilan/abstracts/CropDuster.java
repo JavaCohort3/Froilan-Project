3834d87ed51357a86d9e6fbcc8daac21d1093660
Jermaine34343 <Jermaine34343@gmail.com>
2018-07-17 12:29:57 -0400

    fixed errors in CropDuster

--- src/main/java/io/javacohort3/FarmerFroilan/abstracts/CropDuster.java
+++ src/main/java/io/javacohort3/FarmerFroilan/abstracts/CropDuster.java
@@ -1,9 +1,11 @@
 package io.javacohort3.FarmerFroilan.abstracts;
 
 import io.javacohort3.FarmerFroilan.interfaces.Aircraft;
-import io.javacohort3.FarmerFroilan.classes.CropRow;
+import io.javacohort3.FarmerFroilan.storage.CropRow;
 
 public abstract class CropDuster implements Aircraft {
+    private CropRow cropRow;
+
     public void fertilize(CropRow cropRow){
         this.cropRow = cropRow;
     }
