34d210fad3b6afc4895eb348aef8ae969eb91571
zeeman0306 <zeeman0306@gmail.com>
2018-07-17 12:40:52 -0400

    Added Stable changes

--- src/main/java/io/javacohort3/FarmerFroilan/storage/Stable.java
+++ src/main/java/io/javacohort3/FarmerFroilan/storage/Stable.java
@@ -6,4 +6,27 @@ import java.util.List;
 public class Stable {
     private List<Horse> horses;
     //a stable has a collection of horses
+
+
+    public Stable() {
+    }
+
+    public Stable(List<Horse> horses) {
+        this.horses = horses;
+    }
+
+    public List<Horse> getHorses() {
+        return horses;
+    }
+
+    public void setHorses(List<Horse> horses) {
+        this.horses = horses;
+    }
+
+    @Override
+    public String toString() {
+        return "Stable{" +
+                "horses=" + horses +
+                '}';
+    }
 }
