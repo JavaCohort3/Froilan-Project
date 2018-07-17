eb31649d1760242394012aed49dae3dee6b74043
senorcl0ud <claudemcalpin@gmail.com>
2018-07-17 12:15:23 -0400

    added list import

--- src/main/java/io/javacohort3/FarmerFroilan/storage/FarmHouse.java
+++ src/main/java/io/javacohort3/FarmerFroilan/storage/FarmHouse.java
@@ -1,6 +1,7 @@
 package io.javacohort3.FarmerFroilan.storage;
 
 import io.javacohort3.FarmerFroilan.interfaces.Person;
+import java.util.List;
 
 public class FarmHouse {
     private List<Person> people;
