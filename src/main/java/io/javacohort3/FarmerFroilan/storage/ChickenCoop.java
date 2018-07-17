5036e929e52e3270bf8b2ba9b614e27e41678cc8
senorcl0ud <claudemcalpin@gmail.com>
2018-07-17 12:45:55 -0400

    added constructors getters setters toString

--- src/main/java/io/javacohort3/FarmerFroilan/storage/ChickenCoop.java
+++ src/main/java/io/javacohort3/FarmerFroilan/storage/ChickenCoop.java
@@ -6,4 +6,27 @@ import java.util.List;
 public class ChickenCoop {
     private List<Chicken> chickens;
     //a chickencoop has a collection of chicken
+
+
+    public ChickenCoop() {
+    }
+
+    public ChickenCoop(List<Chicken> chickens) {
+        this.chickens = chickens;
+    }
+
+    public List<Chicken> getChickens() {
+        return chickens;
+    }
+
+    public void setChickens(List<Chicken> chickens) {
+        this.chickens = chickens;
+    }
+
+    @Override
+    public String toString() {
+        return "ChickenCoop{" +
+                "chickens=" + chickens +
+                '}';
+    }
 }
--- src/main/java/io/javacohort3/FarmerFroilan/storage/FarmHouse.java
+++ src/main/java/io/javacohort3/FarmerFroilan/storage/FarmHouse.java
@@ -6,4 +6,26 @@ import java.util.List;
 public class FarmHouse {
     private List<Person> people;
     //a farmhouse has a collection of people
+
+    public FarmHouse() {
+    }
+
+    public FarmHouse(List<Person> people) {
+        this.people = people;
+    }
+
+    public List<Person> getPeople() {
+        return people;
+    }
+
+    public void setPeople(List<Person> people) {
+        this.people = people;
+    }
+
+    @Override
+    public String toString() {
+        return "FarmHouse{" +
+                "people=" + people +
+                '}';
+    }
 }
