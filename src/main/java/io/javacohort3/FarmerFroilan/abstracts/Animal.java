0849a3bec1cb358f2488bf7fa51fa893afb5185f
iVesperX <aermias@live.com>
2018-07-16 23:07:27 -0400

    implemented Eater.java in Animal.java

--- src/main/java/io/javacohort3/FarmerFroilan/abstracts/Animal.java
+++ src/main/java/io/javacohort3/FarmerFroilan/abstracts/Animal.java
@@ -1,6 +1,7 @@
 package io.javacohort3.FarmerFroilan.abstracts;
 
+import io.javacohort3.FarmerFroilan.interfaces.Eater;
 import io.javacohort3.FarmerFroilan.interfaces.NoiseMaker;
 
-public abstract class Animal implements NoiseMaker {
+public abstract class Animal implements Eater, NoiseMaker {
 }
