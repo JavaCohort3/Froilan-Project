dbc6e6740515c29feb864664545a4c2df4688de5
senorcl0ud <claudemcalpin@gmail.com>
2018-07-17 10:08:25 -0400

    added imports for Farm and Farmhouse.

--- src/main/java/io/javacohort3/FarmerFroilan/storage/Farm.java
+++ src/main/java/io/javacohort3/FarmerFroilan/storage/Farm.java
@@ -1,6 +1,9 @@
 package io.javacohort3.FarmerFroilan.storage;
 
 import java.util.List;
+import io.javacohort3.FarmerFroilan.storage.ChickenCoop;
+import io.javacohort3.FarmerFroilan.storage.Stable;
+import io.javacohort3.FarmerFroilan.storage.FarmHouse;
 
 public class Farm {
 
--- src/main/java/io/javacohort3/FarmerFroilan/storage/FarmHouse.java
+++ src/main/java/io/javacohort3/FarmerFroilan/storage/FarmHouse.java
@@ -1,5 +1,7 @@
 package io.javacohort3.FarmerFroilan.storage;
 
+import io.javacohort3.FarmerFroilan.interfaces.Person;
+
 public class FarmHouse {
     private List<Person> people;
     //a farm house has a collection of people
