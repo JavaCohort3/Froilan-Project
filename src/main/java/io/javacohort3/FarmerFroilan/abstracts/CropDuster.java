02765489ca00ec2974ba3759dc6f8309b3712343
Jermaine34343 <Jermaine34343@gmail.com>
2018-07-17 12:55:51 -0400

    fixed CropDuster

--- src/main/java/io/javacohort3/FarmerFroilan/abstracts/CropDuster.java
+++ src/main/java/io/javacohort3/FarmerFroilan/abstracts/CropDuster.java
@@ -4,6 +4,7 @@ import io.javacohort3.FarmerFroilan.interfaces.Aircraft;
 import io.javacohort3.FarmerFroilan.storage.CropRow;
 
 public abstract class CropDuster implements Aircraft {
+
     private CropRow cropRow;
 
     public void fertilize(CropRow cropRow){
